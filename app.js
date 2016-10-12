37adfd438f640ec479198f8f07cfffb3e87e5273
Zhiliang Gong <zhiliang.gong@gmail.com>
2016-10-12 16:52:47 -0500

    updated papers.json by Ben, and starting on the json road

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 var app = angular.module('app',[]);
+
+app.controller('newsSection', function($scope, $http) {
+    $http.get("./data/news.json")
+    .then(function (response) {$scope.news = response.data.news;});
+});
+
+app.controller('peopleSection', function($scope, $http) {
+    $http.get("./data/people.json")
+    .then(function (response) {$scope.people = response.data.people;});
+});
+
+app.controller('researchSection', function($scope, $http) {
+    $http.get("./data/research.json")
+    .then(function (response) {$scope.research = response.data.research;});
+});
