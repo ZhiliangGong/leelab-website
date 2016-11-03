8be82df13331a2355801fbd75f2ec936deb3fffe
Zhiliang Gong <zhiliang.gong@gmail.com>
2016-11-03 14:04:37 -0500

    all data in angular, portraits not working well

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-var app = angular.module('app',[]);
+var app = angular.module('app',['angular.vertilize']);
+
+app.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+});
 
 app.controller('newsSection', function($scope, $http) {
-    $http.get("./data/news.json")
-    .then(function (response) {$scope.news = response.data.news;});
+  $http.get("./data/news.json")
+       .then(function (response) {$scope.news = response.data.news;});
 });
 
 app.controller('peopleSection', function($scope, $http) {
-    $http.get("./data/people.json")
-    .then(function (response) {$scope.people = response.data.people;});
+  $http.get("./data/people.json")
+       .then(function (response) {$scope.people = response.data.people;});
+
 });
 
 app.controller('researchSection', function($scope, $http) {
-    $http.get("./data/research.json")
-    .then(function (response) {$scope.research = response.data.research;});
+  $http.get("./data/research.json")
+       .then(function (response) {$scope.research = response.data.research;});
+});
+
+app.controller('alumniSection', function($scope, $http) {
+  $http.get("./data/alumni.json")
+       .then(function (response) {$scope.alumni = response.data.alumni;});
+});
+
+app.controller('publicationSection', function($scope, $http) {
+  $http.get("./data/papers.json")
+       .then(function (response) {$scope.papers = response.data.papers;});
 });
--- js/lee.js
+++ js/lee.js
@@ -1,41 +1,41 @@
 /*global $*/
 
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+  if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+  } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+  }
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+          scrollTop: $($anchor.attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+      event.preventDefault();
+  });
 });
 
 $(document).ready(function(){
 
-    $('.lee-portrait').matchHeight();
+  $('.lee-portrait').matchHeight();
 
-    //Stack menu when collapsed and show "research fields"
-    $('#research-pill-collapse').on('show.bs.collapse', function() {
-        $('.nav-pills').addClass('nav-stacked');
-    });
+  //Stack menu when collapsed and show "research fields"
+  $('#research-pill-collapse').on('show.bs.collapse', function() {
+      $('.nav-pills').addClass('nav-stacked');
+  });
 
-    //Unstack menu when not collapsed
-    $('#research-pill-collapse').on('hide.bs.collapse', function() {
-        $('.nav-pills').removeClass('nav-stacked');
-    });
+  //Unstack menu when not collapsed
+  $('#research-pill-collapse').on('hide.bs.collapse', function() {
+      $('.nav-pills').removeClass('nav-stacked');
+  });
 
-    $('.pop').on('click', function() {
-      $('.imagepreview').attr('src', $(this).attr('src'));
-  		$('#fm-img-modal').modal('show');
-	  });
+  $('.pop').on('click', function() {
+    $('.imagepreview').attr('src', $(this).attr('src'));
+		$('#fm-img-modal').modal('show');
+  });
 
 });
